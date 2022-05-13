faec9e40a8b5426a0567fcfd4df9e2764fc808dc
kennethongcs <kenneth.ongcs@gmail.com>
2022-05-13 19:44:22 +0800

    base

---
+++ migrations/20220513072754-create-categories-items-tables.js
@@ -0,0 +1,80 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.createTable('items', {
+      id: {
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.createTable('category_items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+      },
+      item_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'items',
+          key: 'id',
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('categories');
+    await queryInterface.dropTable('items');
+    await queryInterface.dropTable('category_items');
+  },
+};
--- models/index.mjs
+++ models/index.mjs
@@ -2,14 +2,26 @@ import { Sequelize } from 'sequelize';
 import allConfig from '../config/config.js';
 
 // import model functions here
+import initCategoryModel from './category.mjs';
+import initItemModel from './item.mjs';
 
 const env = process.env.NODE_ENV || 'development';
 const config = allConfig[env];
 const db = {};
 
-let sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 // add your model definitions to db here
+db.Item = initItemModel(sequelize, Sequelize.DataTypes);
+db.Category = initCategoryModel(sequelize, Sequelize.DataTypes);
+
+db.Item.belongsToMany(db.Category, { through: 'category_items' });
+db.Category.belongsToMany(db.Item, { through: 'category_items' });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
